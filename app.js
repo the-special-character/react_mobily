c4e458a76382f52b1adad2cc6004ed05da929b8e
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-07-30 14:12:01 +0530

    init commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const a = 5;
+
+const b = 10;
+
+
+module.exports = {
+    a,
+    b
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+// import app from './app';
+const app = require("./app.js")
+
+console.log("hello")
+
+console.log(app);
